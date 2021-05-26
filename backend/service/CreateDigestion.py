7bf85e661fc5d2674d1de2ed3bc33891cf90c27d
Felix <felixglinka@gmx.de>
2021-05-26 17:43:19 +0300

    first approach to digest DNA

--- backend/service/CreateDigestion.py
+++ backend/service/CreateDigestion.py
@@ -3,11 +3,24 @@ from DigestedDna import DigestedDna
 
 def digestDna(dnaSequence, restrictionEnzyme):
 
-	dnaFragments = re.split(restrictionEnzyme.cutSite5end + restrictionEnzyme.cutSite3end, dnaSequence)
+	restrictionEnzymeCutSite = restrictionEnzyme.cutSite5end + restrictionEnzyme.cutSite3end
+
+	dnaFragments = re.split(restrictionEnzymeCutSite, dnaSequence)
 
 	return DigestedDna(dnaFragments)
 
 
 def doubleDigestDna(dnaSequence, restrictionEnzyme1, restrictionEnzyme2):
 
-	return DigestedDna([])
+	digestedDnaByFirstCutter = digestDna(dnaSequence, restrictionEnzyme1)
+
+	allDigestedFragements = []
+
+	for fragment in digestedDnaByFirstCutter.fragments:
+		partiallyDigestedDnaBySecondCutter = digestDna(fragment, restrictionEnzyme2)
+		allDigestedFragements.append(partiallyDigestedDnaBySecondCutter.fragments)
+
+	fragmentsFlankedByTwoSites = filter(lambda fragment: fragment.startswith(restrictionEnzyme1.cutSite5end) and fragment.endswith(restrictionEnzyme2.cutSite3end)
+														 or fragment.startswith(restrictionEnzyme2.cutSite5end) and fragment.endswith(restrictionEnzyme1.cutSite3end), allDigestedFragements)
+
+	return DigestedDna(fragmentsFlankedByTwoSites)
