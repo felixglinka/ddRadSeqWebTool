e39828066451bc6c45e3ed14b7578715f3b386a6
Felix <felixglinka@gmx.de>
2021-05-27 18:43:30 +0300

    bug fixing

--- backend/service/CreateDigestion.py
+++ backend/service/CreateDigestion.py
@@ -1,11 +1,13 @@
 import re
+import numpy as np
 from DigestedDna import DigestedDna
 
 def digestDna(dnaSequence, restrictionEnzyme):
 
 	restrictionEnzymeCutSite = restrictionEnzyme.cutSite5end + restrictionEnzyme.cutSite3end
+	restrictionEnzymeCutSitePattern = restrictionEnzyme.cutSite5end + '|' + restrictionEnzyme.cutSite3end
 
-	dnaFragments = re.split(restrictionEnzymeCutSite, dnaSequence)
+	dnaFragments = dnaSequence.replace(restrictionEnzymeCutSite, restrictionEnzymeCutSitePattern).split('|')
 
 	return DigestedDna(dnaFragments)
 
@@ -20,7 +22,9 @@ def doubleDigestDna(dnaSequence, restrictionEnzyme1, restrictionEnzyme2):
 		partiallyDigestedDnaBySecondCutter = digestDna(fragment, restrictionEnzyme2)
 		allDigestedFragments.append(partiallyDigestedDnaBySecondCutter.fragments)
 
-	fragmentsFlankedByTwoSites = filter(lambda fragment: fragment.startswith(restrictionEnzyme1.cutSite5end) and fragment.endswith(restrictionEnzyme2.cutSite3end)
-														 or fragment.startswith(restrictionEnzyme2.cutSite5end) and fragment.endswith(restrictionEnzyme1.cutSite3end), allDigestedFragments)
+	allDigestedFragments = list(np.concatenate(allDigestedFragments).flat)
+
+	fragmentsFlankedByTwoSites = filter(lambda fragment: fragment.startswith(restrictionEnzyme1.cutSite3end) and fragment.endswith(restrictionEnzyme2.cutSite5end)
+														 or fragment.startswith(restrictionEnzyme2.cutSite3end) and fragment.endswith(restrictionEnzyme1.cutSite5end), allDigestedFragments)
 
 	return DigestedDna(fragmentsFlankedByTwoSites)
