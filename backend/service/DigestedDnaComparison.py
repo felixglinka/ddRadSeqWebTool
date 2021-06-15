2e1bae388971a969f879a687c5d6d92636770461
Felix <felixglinka@gmx.de>
2021-06-15 14:22:36 +0300

    corrected binning plus highlighting

--- backend/service/DigestedDnaComparison.py
+++ backend/service/DigestedDnaComparison.py
@@ -29,7 +29,7 @@ class DigestedDnaComparison:
     plt.xlabel('Fragment size bin (bp)')
     plt.ylabel('Number of digested fragments')
     plt.xticks(np.arange(0, MAX_GRAPH_VIEW + 20, step=20),
-               labels=['0-10', '200-210', '400-410', '600-610', '800-810', '1000-1010'])
+               labels=['(0-10]', '(200-210]', '(400-410]', '(600-610]', '(800-810]', '(1000-1010]'])
     plt.legend(bbox_to_anchor=(1.04, 1), loc='upper left')
 
     if selectedMinSize !=None and selectedMaxSize != None \
@@ -57,7 +57,9 @@ class DigestedDnaComparison:
                  self.digestedDna2.countFragmentsInGivenRange(selectedMinSize, selectedMaxSize)),
                bbox={'facecolor': 'khaki', 'alpha': 0.25})
 
-      plt.axvspan(selectedMinSize/10 - ((selectedMinSize/10)%10), selectedMaxSize/10 - ((selectedMaxSize/10)%10), color='khaki', alpha=0.5)
+      plt.axvspan(((selectedMinSize-1) - ((selectedMinSize-1)%10))/10 if selectedMinSize > 0 else 0,
+                  ((selectedMaxSize-1) - ((selectedMaxSize-1)%10))/10 if selectedMaxSize > 0 else 0,
+                  color='khaki', alpha=0.5)
 
     buffer = io.BytesIO()
     plt.savefig(buffer, format='png',bbox_inches='tight')
