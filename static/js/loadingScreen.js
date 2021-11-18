842e0f6a01fe98eb3e5dab8fb307ec5454cb1473
Felix <felixglinka@gmx.de>
2021-11-18 12:16:34 +0200

    loading

--- static/js/loadingScreen.js
+++ static/js/loadingScreen.js
@@ -121,33 +121,10 @@ function showLoading() {
         document.getElementById('dataFrame').style.display="none";
     }
 
-    window.location.hash = '#loader';
-}
+    $('html, body').animate({
+      scrollTop: $("#loader").offset().top
+    }, 0.5);
 
-function fillFastaUploader() {
-
-    document.getElementById("submitButton").addEventListener('click', function(e) {
-        const size = 40000;
-        let reader = new FileReader();
-        let buf;
-        let file = document.getElementById('fastaFileUpload').files[0];
-        reader.onload = function(e) {
-            buf = new Uint8Array(e.target.result);
-            for (let i = 0;  i < buf.length; i += size) {
-                let fd = new FormData();
-                fd.append('fname', [file.name, i+1, 'of', buf.length].join('-'));
-                fd.append('data', new Blob([buf.subarray(i, i + size)]));
-                let oReq = new XMLHttpRequest();
-                oReq.open("POST", '', true);
-                oReq.onload = function (oEvent) {
-                   // Uploaded.
-                };
-                oReq.send(fd);
-            }
-        }
-
-        reader.readAsArrayBuffer(file);
-    });
 }
 
 window.addEventListener('load', fillFastaUploader)
