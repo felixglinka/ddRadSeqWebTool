203b218734eea709ed5f9015a008cb3fa3d69cfb
Felix <felixglinka@gmx.de>
2022-01-05 12:37:43 +0200

    beginn updating the clean up

--- backend/service/CleanUpService.py
+++ backend/service/CleanUpService.py
@@ -1,3 +1,9 @@
-def cleanUp():
-    print('hi')
+from datetime import datetime
 
+def cleanUp():
+    currentTime = datetime.now()
+    currentYear = currentTime.year
+    currentMonth = currentTime.month
+    currentDay = currentTime.day
+
+    print(currentYear, currentMonth, currentDay)
--- backend/service/Update.py
+++ backend/service/Update.py
@@ -3,6 +3,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from backend.service.CleanUpService import cleanUp
 
 def start():
-    scheduler = BackgroundScheduler()
+    scheduler = BackgroundScheduler(timezone='UTC')
     scheduler.add_job(cleanUp, 'interval', seconds=10)
     scheduler.start()
