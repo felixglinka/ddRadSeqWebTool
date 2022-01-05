10ed6d496fdd2bf8cfa9ad49b43b6054db4ae6c9
Felix <felixglinka@gmx.de>
2022-01-05 10:03:34 +0200

    begin clean up job

---
+++ backend/service/CleanUpService.py
@@ -0,0 +1,3 @@
+def cleanUp():
+    print('hi')
+
---
+++ backend/service/Update.py
@@ -0,0 +1,8 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from backend.service.CleanUpService import cleanUp
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(cleanUp, 'interval', seconds=10)
+    scheduler.start()
