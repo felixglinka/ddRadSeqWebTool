517d6a10163360b0dd3918ab18c8cc60c76bd35e
Felix <felixglinka@gmx.de>
2022-01-10 13:24:55 +0200

    updates cheduler

--- api/scheduler.py
+++ api/scheduler.py
@@ -55,6 +55,6 @@ def start():
     # - Add a scheduled job to the job store on application initialization
     # - The job will execute a model class method at midnight each day
     # - replace_existing in combination with the unique ID prevents duplicate copies of the job
-    scheduler.add_job(cleanUp, trigger=CronTrigger(hour=11, minute=22), id="my_class_method",replace_existing=True)
+    scheduler.add_job(cleanUp, trigger=CronTrigger(hour=11, minute=26), id="my_class_method",replace_existing=True)
 
     scheduler.start()
