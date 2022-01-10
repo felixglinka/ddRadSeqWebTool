2989215f135bf55c4382dd161e5c381639e4873c
Felix <felixglinka@gmx.de>
2022-01-10 13:14:55 +0200

    updates cheduler

---
+++ api/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+class CoreConfig(AppConfig):
+    name = "api"
+
+    def ready(self):
+        from . import scheduler
+        if settings.SCHEDULER_AUTOSTART:
+        	scheduler.start()
