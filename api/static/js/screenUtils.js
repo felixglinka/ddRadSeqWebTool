5da6692318e6adf88f0b16d8c13a80ea80520a80
Felix <felixglinka@gmx.de>
2022-06-27 14:29:48 +0300

    bug fix

--- api/static/js/screenUtils.js
+++ api/static/js/screenUtils.js
@@ -77,7 +77,7 @@ function getResultsOnPage(){
         }, 0.5);
     }
 
-    if (typeof noRecommendation === 'n') {
+    if (noRecommendation === 'n') {
         $('html, body').animate({
           scrollTop: $("#noRecommendation").offset().top
         }, 0.5);
